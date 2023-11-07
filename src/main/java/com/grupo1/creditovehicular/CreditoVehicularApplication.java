338ef0ad1525937a6bc46a0b6f33dd5f60105b7c
Leonardo Aquino <u20201b949@upc.edu.pe>
2023-11-06 22:47:21 -0500

    Initial commit

---
+++ src/main/java/com/grupo1/creditovehicular/CreditoVehicularApplication.java
@@ -0,0 +1,13 @@
+package com.grupo1.creditovehicular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditoVehicularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditoVehicularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grupo1/creditovehicular/CreditoVehicularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.creditovehicular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditoVehicularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
